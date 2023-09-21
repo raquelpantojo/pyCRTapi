e00383ae482887722ca5fe7ca779707624c4fec3
raquelpantojo <32850913+raquelpantojo@users.noreply.github.com>
2023-09-21 18:27:01 -0300

    apload aoo.py

--- app.py
+++ app.py
@@ -8,10 +8,37 @@ from process_video import process_video  # Importe a função process_video do s
 uploads_dir = "uploads"
 os.makedirs(uploads_dir, exist_ok=True)
 
+
+
+# Função para capturar a imagem da webcam
+def capturar_imagem():
+    cap = cv2.VideoCapture(0)  # 0 representa a câmera padrão (geralmente a webcam)
+    
+    if not cap.isOpened():
+        st.error("Não foi possível acessar a webcam.")
+        return
+
+    st.write("Pressione o botão para capturar uma imagem da webcam")
+    if st.button("Capturar"):
+        ret, frame = cap.read()
+        if ret:
+            # Exibe a imagem capturada
+            st.image(frame, channels="BGR", use_column_width=True, caption="Imagem Capturada")
+            # Salva a imagem capturada em um arquivo (opcional)
+            cv2.imwrite("imagem_capturada.jpg", frame)
+        else:
+            st.error("Não foi possível capturar a imagem.")
+
+        cap.release()
+
+capturar_imagem()
+
+
+
 # Função para verificar se há imagens de pele em um vídeo
 def verifica_imagens_de_pele(video):
     cap = cv2.VideoCapture(video.name)
-    tem_imagem_de_pele = False
+    tem_imagem_de_pele = True
     imagem_ycrcb = None  # Inicialize a variável imagem_ycrcb com None
     imagem_cinza = None
     imagem_hsv = None
@@ -29,7 +56,7 @@ def verifica_imagens_de_pele(video):
         imagem_cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         # Converta a imagem para o espaço de cores YCrCb
         #Converta a imagem para o espaço de cores HSV
-        #imagem_hsv = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
+        imagem_hsv = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
 
         # Defina os valores de limite para a cor da pele no espaço HSV
         lower_hsv = np.array([0, 20, 80], dtype="uint8")
@@ -70,50 +97,52 @@ def verifica_imagens_de_pele(video):
 
     cap.release()
 
-    return tem_imagem_de_pele, skin, imagem_cinza, imagem_ycrcb  # Retorna também a skin_mask e a imagem_ycrcb
+    return tem_imagem_de_pele, skin, imagem_hsv, imagem_ycrcb  # Retorna também a skin_mask e a imagem_ycrcb
 
-# Função para converter a máscara de pele em uma imagem RGB
-def skin_mask_to_rgb_image(skin_mask):
-    skin_mask_rgb = cv2.cvtColor(skin_mask, cv2.COLOR_BGR2RGB)
-    return skin_mask_rgb
 
 # Interface do Streamlit
-st.title("Detecção de pyCRT por vídeo")
+st.title("Detecção de pyCRT")
 st.write("Carregue um arquivo de vídeo para realizar o teste do CRT.")
 
-uploaded_file = st.file_uploader("Carregar vídeo", type=["mp4", "avi", "wmv"])
-if uploaded_file is not None:
-    video_path = os.path.join(uploads_dir, uploaded_file.name)
-    with open(video_path, "wb") as f:
-        f.write(uploaded_file.read())  # Salva o arquivo enviado pelo usuário
+opcao = st.radio("Selecione uma opção:", ("Tirar uma Foto da Câmera", "Enviar Vídeo Existente"))
 
-    # Verifique as imagens de pele e processe o vídeo
-    tem_pele, mascara_final, imagem_hsv, imagem_ycrcb = verifica_imagens_de_pele(uploaded_file)
-    if tem_pele == False:
-        st.write("Imagens de pele foram encontradas.")
-        st.video(uploaded_file)
-        
-        if imagem_ycrcb is not None:
-            st.image(imagem_ycrcb, channels="BGR", use_column_width=True, caption="Imagem no espaço YCrCb")
+if opcao == "Fazer um video":
+    img_file_buffer = st.camera_input("Tirar uma foto")
+    if img_file_buffer is not None:
+        st.image(img_file_buffer, use_column_width=True, caption="Imagem Capturada")
+        # Você pode processar a imagem aqui se necessário.
+else:
+    uploaded_file = st.file_uploader("Carregar vídeo", type=["mp4", "avi", "wmv"])
+    if uploaded_file is not None:
+        video_path = os.path.join(uploads_dir, uploaded_file.name)
+        with open(video_path, "wb") as f:
+            f.write(uploaded_file.read())  # Salva o arquivo enviado pelo usuário
+        # Verifique as imagens de pele e processe o vídeo
+        tem_pele, mascara_final, imagem_hsv, imagem_ycrcb = verifica_imagens_de_pele(uploaded_file)
+        if tem_pele:
+            st.write("Imagens de pele foram encontradas.")
+            st.video(video_path)
 
-        st.write("Processando vídeo...")
-        processed_data = process_video(video_path)  # Processar o vídeo
-        st.write(f"Resultados do processamento: {processed_data}")
+            st.write("Processando vídeo...")
+            processed_data = process_video(video_path)  # Processar o vídeo
+            st.write(f"Resultados do processamento: {processed_data}")
 
-        # Capturar e exibir o quadro 50 no espaço YCrCb
-        cap = cv2.VideoCapture(video_path)
-        frame_number = 50  # Número do quadro desejado
-        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
-        ret, frame = cap.read()
-        if ret:
-            frame_ycrcb = cv2.cvtColor(frame, cv2.COLOR_BGR2YCrCb)
-            st.image(frame_ycrcb, channels="BGR", use_column_width=True, caption=f"Quadro {frame_number} em YCrCb")
+            # Capturar e exibir o quadro 50 no espaço YCrCb
+            cap = cv2.VideoCapture(video_path)
+            frame_number = 50  # Número do quadro desejado
+            cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+            ret, frame = cap.read()
+            if ret:
+                frame_ycrcb = cv2.cvtColor(frame, cv2.COLOR_BGR2YCrCb)
+                st.image(frame_ycrcb, channels="BGR", use_column_width=True, caption=f"Quadro {frame_number} em YCrCb")
 
-        cap.release()
+            cap.release()
 
-    else:
-        st.write("Não foram encontradas imagens de pele.")
-        st.video(uploaded_file)
+        else:
+            st.write("Não foram encontradas imagens de pele.")
+            st.video(video_path)
+
+
 
 
 # Exibir os logos no rodapé com o texto "Desenvolvido por:" e fundo preto
@@ -130,8 +159,8 @@ st.markdown(
 )
 
 st.sidebar.write("Desenvolvido por:")
-st.sidebar.image("logo_lab.png", use_column_width=True, width=150)
-st.sidebar.image("logo_usp.png", use_column_width=True, width=150)
+st.sidebar.image("Imagens_logo\\logo_lab.png", use_column_width=True, width=150)
+st.sidebar.image("Imagens_logo\\logo_usp.png", use_column_width=True, width=150)
 
 # Adicionar um link no sidebar
 st.sidebar.markdown(
