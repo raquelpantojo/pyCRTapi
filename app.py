96d5837cea8ef42febe55a98b406c8987ee50a51
raquelpantojo <raquel.pantojo.souza@usp.br>
2023-09-21 23:04:55 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,13 @@
-"""
-atualizado dia 21-09 
-
-
-"""
-
 import streamlit as st
 import os
 import cv2
 import numpy as np
-from process_video import process_video  # Importe a função process_video do seu módulo
 import threading
-
+from process_video import process_video  # Importe a função process_video do seu módulo
 
 # Configurar diretório de upload
 uploads_dir = "uploads"
 os.makedirs(uploads_dir, exist_ok=True)
-############################################################
 
 # Global variable for the stop event
 stop_event = threading.Event()
@@ -25,23 +17,23 @@ def capture_video(camera_index, output_filename):
     cap = cv2.VideoCapture(camera_index)
     frame_width = int(cap.get(3))
     frame_height = int(cap.get(4))
-    
+
     # Define the codec and create a VideoWriter object
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter(output_filename, fourcc, 20.0, (frame_width, frame_height))
-    
+
     while not stop_event.is_set():
         ret, frame = cap.read()
         if not ret:
             st.error("Error capturing video.")
             break
-        
+
         # Display the frame in Streamlit
         st.image(frame, channels="BGR", use_column_width=True)
-        
+
         # Write the frame to the video file
         out.write(frame)
-    
+
     cap.release()
     out.release()
 
@@ -69,9 +61,6 @@ if stop_button:
 if os.path.exists(output_filename):
     st.write("Captured Video:")
     st.video(output_filename)
-
-
-
 
 # Função para verificar se há imagens de pele em um vídeo
 def verifica_imagens_de_pele(video):
@@ -120,48 +109,32 @@ def verifica_imagens_de_pele(video):
 
         # Aplique a máscara para detectar a pele no espaço YCrCb
         mascara_ycrcb = cv2.inRange(imagem_ycrcb, lower_ycrcb, upper_ycrcb)
-        
 
         # Combine as duas máscaras resultantes usando uma operação AND
         mascara_combinada = cv2.bitwise_and(mascara_hsv, mascara_ycrcb)
 
-        #limite_percentagem_pele = 1  # Defina um limite de porcentagem aqui
-        print(f"imagem_hsv shape: {imagem_hsv.shape}")
-        print(f"imagem_ycrcb shape: {imagem_ycrcb.shape}")          
         if np.count_nonzero(skinMask) > 0:
             tem_imagem_de_pele = True
             break
-                
 
     cap.release()
 
     return tem_imagem_de_pele, skin, imagem_hsv, imagem_ycrcb  # Retorna também a skin_mask e a imagem_ycrcb
 
-
 # Interface do Streamlit
 st.title("Detecção de pyCRT")
 st.write("Carregue um arquivo de vídeo para realizar o teste do CRT.")
 
-opcao = st.radio("Selecione uma opção:", ("Fazer um video", "Enviar Vídeo Existente"))
+opcao = st.radio("Selecione uma opção:", ("Fazer um vídeo", "Enviar Vídeo Existente"))
 
-camera_index = st.camera_input("Fazer um vídeo")  # Pode escolher entre diferentes câmeras
+if opcao == "Fazer um vídeo":
+    camera_index = st.selectbox("Select Camera", (0, 1, 2, 3))  # Selecione a câmera desejada
+    if st.button("Iniciar Gravação"):
+        output_filename = f"video_capturado_camera_{camera_index}.avi"
+        capture_video(camera_index, output_filename)  # Inicie a gravação
 
-if opcao == "Fazer um video":
-        if st.button("Iniciar Gravação", key=f"start_button_{camera_index}"):
-            output_filename = f"video_capturado_camera_{camera_index}.avi"
-            capturar_video(camera_index, output_filename)  # Inicie a gravação
-
-            # Após a gravação, exiba o vídeo gravado
-            st.video(output_filename)
-        # Verifique se camera_index não é None antes de chamar capturar_video
-        if camera_index is not None:
-            output_filename = f"video_capturado_camera_{camera_index}.avi"
-            
-            if st.button("Iniciar Gravação"):
-                capturar_video(camera_index, output_filename)  # Chame a função para gravar o vídeo
-
-                # Após a gravação, exiba o vídeo gravado
-                st.video(output_filename)
+        # Após a gravação, exiba o vídeo gravado
+        st.video(output_filename)
 
 else:
     uploaded_file = st.file_uploader("Carregar vídeo", type=["mp4", "avi", "wmv"])
@@ -169,6 +142,7 @@ else:
         video_path = os.path.join(uploads_dir, uploaded_file.name)
         with open(video_path, "wb") as f:
             f.write(uploaded_file.read())  # Salva o arquivo enviado pelo usuário
+
         # Verifique as imagens de pele e processe o vídeo
         tem_pele, mascara_final, imagem_hsv, imagem_ycrcb = verifica_imagens_de_pele(uploaded_file)
         if tem_pele:
@@ -193,9 +167,6 @@ else:
         else:
             st.write("Não foram encontradas imagens de pele.")
             st.video(video_path)
-
-
-
 
 # Exibir os logos no rodapé com o texto "Desenvolvido por:" e fundo preto
 st.markdown(
