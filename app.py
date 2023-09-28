84b5d11943febda3f4280141b09936d445686327
raquelpantojo <32850913+raquelpantojo@users.noreply.github.com>
2023-09-28 15:01:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,18 +8,22 @@ def main():
     option = st.radio("Selecione uma opção:", ("Fazer um vídeo", "Enviar Vídeo Existente"))
 
     if option == "Fazer um vídeo":
-        camera_index = st.camera_input("Selecione uma câmera")  # Escolha a câmera
+        camera_index = st.camera_input("Câmera:")  # Escolha a câmera
 
         st.write("Iniciando a captura de vídeo. Aguarde...")
 
         cap = cv2.VideoCapture(camera_index)
 
+        if not cap.isOpened():
+            st.write("Erro ao iniciar a câmera. Verifique a configuração da câmera.")
+            return
+
         out = None
         recording = False
 
-        record_button = st.button("Iniciar Gravação")  # Botão para iniciar e parar a gravação
+        start_stop_button = st.button("Iniciar Gravação" if not recording else "Parar Gravação")  # Botão para iniciar e parar a gravação
 
-        if record_button:
+        if start_stop_button:
             recording = not recording
             if recording:
                 out = cv2.VideoWriter('video1.avi', cv2.VideoWriter_fourcc(*'XVID'), 24.0, (1280, 720))
@@ -38,8 +42,18 @@ def main():
 
         cap.release()
 
+        if out:
+            st.write("Gravação concluída. Clique no botão abaixo para ver o vídeo.")
+            if st.button("Mostrar Vídeo Gravado"):
+                show_video('video1.avi')
+
     elif option == "Enviar Vídeo Existente":
         st.write("Opção 'Enviar Vídeo Existente' selecionada. Faça o upload de um vídeo existente.")
+
+def show_video(video_path):
+    video_file = open(video_path, 'rb')
+    video_bytes = video_file.read()
+    st.video(video_bytes)
 
 if __name__ == "__main__":
     main()
