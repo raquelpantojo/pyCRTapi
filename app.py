1e4ed1626134e5ec4e1382e99984f5115a0d28f5
raquelpantojo <32850913+raquelpantojo@users.noreply.github.com>
2023-09-28 14:41:50 -0300

    Update app.py
    mofiquei para ficar parecido com a tela que foi desenvolvida em python com kivy

--- app.py
+++ app.py
@@ -1,256 +1,66 @@
-"""
-atualizado dia 21-09 
-
-dicas: https://github.com/whitphx/streamlit-webrtc
-https://bgremoval.streamlit.app/
-
-"""
-
 import streamlit as st
-import os
 import cv2
+import os
+from pyCRT import PCRT
 import numpy as np
-from process_video import process_video  # Importe a função process_video do seu módulo
-import threading
+import time
 
+# Função para iniciar a captura de vídeo
+def start_capture():
+    cap = cv2.VideoCapture(0)
+    out = cv2.VideoWriter('video1.avi', cv2.VideoWriter_fourcc(*'XVID'), 24.0, (1280, 720))
 
-from rembg import remove
-from PIL import Image
-from io import BytesIO
-import base64
+    st.image(np.zeros((1, 1, 3)))  # Placeholder para o vídeo
 
+    st.write("Iniciando captura de vídeo. Aguarde...")
 
-import subprocess
-
-
-
-# Define o layout da página
-st.set_page_config(
-    page_title="Cálculo do Tempo de Enchimento capilar",
-    page_icon=":information_source:",
-    layout="wide"
-)
-
-# Use HTML para criar um layout personalizado
-st.markdown("""
-    <style>
-        .container {
-            display: flex;
-            justify-content: space-between;
-        }
-        .title {
-            font-size: 36px;
-        }
-        .menu {
-            margin-top: 25px;
-            margin-right: 20px;
-        }
-    </style>
-    <div class="container">
-        <div class="title">Cálculo do Tempo de Enchimento capilar</div>
-        <div class="menu">
-            <a href="#sobre">Sobre</a>
-        </div>
-    </div>
-""", unsafe_allow_html=True)
-
-# Ancoragem para a seção "Sobre"
-st.markdown("<a id='sobre'></a>", unsafe_allow_html=True)
-
-# Conteúdo da seção "Sobre"
-st.header("Sobre")
-st.write("Desenvolvido por:")
-st.image("logo_lab.png", use_column_width=True, width=50)
-st.image("logo_usp.png", use_column_width=True, width=50)
-
-# Adicionar um link no sidebar
-st.markdown(
-    """
-[Visite nosso site](https://sites.usp.br/photobiomed/)
-"""
-)
-
-
-
-
-
-
-# Configurar diretório de upload
-uploads_dir = "uploads"
-os.makedirs(uploads_dir, exist_ok=True)
-
-
-
-# Global variable for the stop event
-stop_event = threading.Event()
-
-
-MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
-
-# Download the fixed image
-def convert_image(img):
-    buf = BytesIO()
-    img.save(buf, format="PNG")
-    byte_im = buf.getvalue()
-    return byte_im
-
-#Remover background
-def remove_image(upload):
-    image = Image.open(upload, resample=Image.LANCZOS)
-    #st.write("Original Image :camera:")
-    #st.image(image)
-
-    fixed = remove(image)
-    st.write("Imagem com Fundo Removido :wrench:")
-    st.image(fixed)
-
-
-
-# Function to capture video from the camera
-def capturar_video(camera_index, output_filename):
-    cap = cv2.VideoCapture(camera_index)
-    
-    if not cap.isOpened():
-        st.error(f"Não foi possível acessar a câmera {camera_index}.")
-        return
-
-output_filename = None  # Inicialize a variável global    
-
-# Função para verificar se há imagens de pele em um vídeo
-def verifica_imagens_de_pele(video):
-    cap = cv2.VideoCapture(video.name)
-    tem_imagem_de_pele = True
-    imagem_ycrcb = None  # Inicialize a variável imagem_ycrcb com None
-    imagem_cinza = None
-    imagem_hsv = None
-    skinMask = None
-    skin = None
-    mascara_hsv = None
+    number = 0
+    textmsg = ""
+    increment_time = 0.1
+    roi = (562, 332, 150, 161)  # Região de interesse na imagem da câmera
 
     while True:
         ret, frame = cap.read()
+        out.write(frame)
 
-        if not ret:
+        st.image(frame, use_column_width=True)
+
+        st.write(f"Cronômetro: {number:.1f}")
+        if number > 2 and number <= 4:
+            textmsg = 'Posicione o dedo indicador no círculo'
+        if number > 5 and number <= 11:
+            textmsg = 'Aperte'
+        if number >= 11:
+            textmsg = 'Libere'
+        if number >= 12:
+            textmsg = 'Aguarde'
+        if number >= 16:
+            textmsg = 'Calcule o CRT'
+            # Interrompa a captura aqui e faça o cálculo do CRT
+            pycrt = PCRT.fromVideoFile("video1.avi", roi=roi, displayVideo=False, exclusionCriteria=0.6)
+            pycrtvalue = round(pycrt.pCRT[0], 2)
+            pycrtincert = round(pycrt.pCRT[1], 2)
+
+            st.write(f"pCRT: {pycrtvalue} s")
+            st.write(f"Incerteza: {pycrtincert} s")
+
             break
 
-        # Converta o frame para escala de cinza para simplificar a detecção de pele
-        imagem_cinza = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # Converta a imagem para o espaço de cores YCrCb
-        #Converta a imagem para o espaço de cores HSV
-        imagem_hsv = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
-
-        # Defina os valores de limite para a cor da pele no espaço HSV
-        lower_hsv = np.array([0, 20, 80], dtype="uint8")
-        upper_hsv = np.array([255, 255, 255], dtype="uint8")
-
-        # Aplique a máscara para detectar a pele no espaço HSV
-        mascara_hsv = cv2.inRange(imagem_hsv, lower_hsv, upper_hsv)
-
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (12, 12))
-        skinMask = cv2.erode(mascara_hsv, kernel, iterations=3)
-        skinMask = cv2.dilate(mascara_hsv, kernel, iterations=3)
-
-        # blur the mask to help remove noise, then apply the
-        # mask to the frame
-        skinMask = cv2.GaussianBlur(skinMask, (13, 13), 5)
-        skin = cv2.bitwise_and(frame, frame, mask=skinMask)
-
-        imagem_ycrcb = cv2.cvtColor(frame, cv2.COLOR_BGR2YCrCb)
-
-        # Defina os valores de limite para a cor da pele no espaço YCrCb
-        lower_ycrcb = np.array([0, 136, 0], dtype="uint8")
-        upper_ycrcb = np.array([255, 173, 127], dtype="uint8")
-
-        # Aplique a máscara para detectar a pele no espaço YCrCb
-        mascara_ycrcb = cv2.inRange(imagem_ycrcb, lower_ycrcb, upper_ycrcb)
-        
-
-        # Combine as duas máscaras resultantes usando uma operação AND
-        mascara_combinada = cv2.bitwise_and(mascara_hsv, mascara_ycrcb)
-
-    
-        #print(f"imagem_hsv shape: {imagem_hsv.shape}")
-        tem_pixels = np.any(mascara_combinada)
-        #print(f"imagem_ycrcb shape: {imagem_ycrcb.shape}")          
-        if tem_pixels:
-            tem_imagem_de_pele = True
-            break
-                
+        time.sleep(increment_time)
+        number += increment_time
 
     cap.release()
 
-    return tem_imagem_de_pele  # Retorna também a skin_mask e a imagem_ycrcb
+# Função principal do Streamlit
+def main():
+    st.title("Aplicativo de Captura de Vídeo")
+    st.sidebar.header("Configurações")
 
+    video_resolution = st.sidebar.selectbox("Resolução de Vídeo", ["480p", "720p", "1080p", "4k"])
+    st.write(f"Resolução selecionada: {video_resolution}")
 
-# Interface do Streamlit
-st.title("Detecção de pyCRT")
-st.write("Carregue um arquivo de vídeo para realizar o teste do CRT.")
+    if st.button("Iniciar Captura de Vídeo"):
+        start_capture()
 
-opcao = st.radio("Selecione uma opção:", ("Fazer um video", "Enviar Vídeo Existente"))
-
-camera_index = st.camera_input("Fazer um vídeo")  # Pode escolher entre diferentes câmeras
-
-if opcao == "Fazer um video":
-        # Fazer video:
-        cap = cv2.VideoCapture(camera_index)
-        if cap.isOpened():
-            ret, frame = cap.read()
-            if ret:
-                st.image(frame, channels="BGR", use_column_width=True)
-        cap.release()
-
-        if st.button("Iniciar Gravação", key=f"start_button_{camera_index}"):
-            output_filename = f"video_capturado_camera_{camera_index}.avi"
-            capturar_video(camera_index, output_filename)  # Inicie a gravação
-
-            # Após a gravação, exiba o vídeo gravado
-            st.write("Vídeo Capturado:")
-            st.video(output_filename)
-            
-        # Verifique se camera_index não é None antes de chamar capturar_video
-        if camera_index is not None:
-            output_filename = f"video_capturado_camera_{camera_index}.avi"
-            
-            if st.button("Iniciar Gravação"):
-                capturar_video(camera_index, output_filename)  # Chame a função para gravar o vídeo
-
-                # Após a gravação, exiba o vídeo gravado
-                st.write("Vídeo Capturado:")
-                st.video(output_filename)
-
-else:
-    uploaded_file = st.file_uploader("Carregar vídeo", type=["mp4", "avi", "wmv"])
-    if uploaded_file is not None:
-        video_path = os.path.join(uploads_dir, uploaded_file.name)
-        with open(video_path, "wb") as f:
-            f.write(uploaded_file.read())  # Salva o arquivo enviado pelo usuário
-        # Verifique as imagens de pele e processe o vídeo
-        tem_pele = verifica_imagens_de_pele(uploaded_file)
-        
-        if tem_pele == True:
-            st.write("Imagens de pele foram encontradas.")
-            st.video(video_path)
-            
-
-            st.write("Processando vídeo...")
-            processed_data = process_video(video_path)  # Processar o vídeo
-            st.write(f"Resultados do processamento: {processed_data}")
-
-            # Capturar e exibir o quadro 50 no espaço YCrCb
-            cap = cv2.VideoCapture(video_path)
-            frame_number = 50  # Número do quadro desejado
-            cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
-            ret, frame = cap.read()
-            
-            # Remover o fundo do vídeo
-            #output_video_path = "video_com_fundo_removido"  # Especifique o caminho de saída desejado
-            remove_image(frame)  # Chame a função para remover o fundo do vídeo
-
-            #st.write("Vídeo com fundo removido:")
-            #st.image(output_video_path)
-                    
-            
-        else:
-            st.write("Imagens de pele não foram encontradas, envie um novo vídeo")
-    
-
+if __name__ == "__main__":
+    main()
