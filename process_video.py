c89edc887f87a667e74802f7c95d06011d464caa
RPSB <raquel.pantojo.souza@usp.br>
2023-10-16 16:35:44 -0300

    Update process_video.py

--- process_video.py
+++ process_video.py
@@ -19,14 +19,54 @@ def process_video(video_path,roi):
    pycrtvalue = pcrt.pCRT[0].__round__(2) 
    pycrtincert = pcrt.pCRT[1].__round__(2) 
     
-   # Gerar gráfico do canal verde
-    plt.figure()
-    plt.imshow(pcrt.video.data[:, :, 1], cmap='Greens')  # Use the green channel for the image
-    plt.colorbar()
-    plt.title('Green Channel')
-    
-    # Exibir o gráfico no Streamlit
-    st.pyplot()
-   
+
+
+   # Inicialize a captura de vídeo
+   cap = cv2.VideoCapture(video_path)
+
+   # Inicialize listas para armazenar os valores médios dos canais RGB
+   mean_red_values = []
+   mean_green_values = []
+   mean_blue_values = []
+
+   while True:
+      ret, frame = cap.read()
+      if not ret:
+         break
+
+      # Divida o quadro em canais RGB
+      b, g, r = cv2.split(frame)
+
+      # Calcule a média dos canais RGB e adicione à lista
+      mean_red = np.mean(r)
+      mean_green = np.mean(g)
+      mean_blue = np.mean(b)
+
+      mean_red_values.append(mean_red)
+      mean_green_values.append(mean_green)
+      mean_blue_values.append(mean_blue)
+
+   # Crie uma lista de tempo (em segundos) com base no número de quadros
+   frame_count = len(mean_red_values)
+   frame_rate = int(cap.get(cv2.CAP_PROP_FPS))
+   time_values = [i / frame_rate for i in range(frame_count)]
+
+   # Plote os valores médios ao longo do tempo
+   plt.plot(time_values, mean_red_values, 'r', label='Canal R')
+   plt.plot(time_values, mean_green_values, 'g', label='Canal G')
+   plt.plot(time_values, mean_blue_values, 'b', label='Canal B')
+
+   # Configuração do gráfico
+   plt.xlabel('Tempo (s)')
+   plt.ylabel('Valor Médio')
+   plt.title('Valores Médios dos Canais RGB ao Longo do Tempo')
+   plt.legend()
+
+   # Exibir o gráfico
+   plt.show()
+
+   # Libere a captura de vídeo
+   cap.release()
+
     
    return {'pCRT': pycrtvalue, 'incerteza': pycrtincert}
