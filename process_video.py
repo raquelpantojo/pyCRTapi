6f2bb258490a9d2c5b8f1c57c923acd2604cfa0c
RPSB <raquel.pantojo.souza@usp.br>
2023-10-16 16:19:48 -0300

    Update process_video.py

--- process_video.py
+++ process_video.py
@@ -13,16 +13,8 @@ def process_video(video_path,roi,xo1, yo1, xo2, yo2):
     #pcrt = PCRT.fromVideoFile(video_path)
    pcrt = PCRT.fromVideoFile(video_path,roi=roi,displayVideo=False,exclusionMethod='best fit',exclusionCriteria=9999999)
     
-    #(1.9553732602812774, 0.15108928992342496)
+   #(1.9553732602812774, 0.15108928992342496)
    
-    # Redimensione a imagem para o tamanho desejado
-   new_width = 200  # Largura desejada
-   new_height = 200  # Altura desejada
-   # Exiba a imagem redimensionada
-   roiopencv = frame[yo1:yo2, xo1:xo2]
-   roi_resized_opencv = cv2.resize(roiopencv, (new_width, new_height)) 
-   st.image(roi_resized_opencv, channels="BGR") 
-    
    # Get the values you want from the pcrt object
    pycrtvalue = pcrt.pCRT[0].__round__(2) 
    pycrtincert = pcrt.pCRT[1].__round__(2) 
