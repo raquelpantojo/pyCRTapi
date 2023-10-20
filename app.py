b459fd7e3a8c7c72824e87a6211e0b153a7322c6
RPSB <raquel.pantojo.souza@usp.br>
2023-10-20 15:56:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -159,10 +159,10 @@ def verifica_imagens_de_pele(video):
 
 # Interface do Streamlit
 st.write("Carregue um arquivo de vídeo para realizar o teste do CRT.")
-opcao = st.radio("Selecione uma opção:", ("Fazer um video", "Enviar Vídeo Existente"))
+#opcao = st.radio("Selecione uma opção:", ("Fazer um video", "Enviar Vídeo Existente"))
 
-if opcao == "Fazer um video":
-    st.write("Ainda não é possivel fazer imagens com sua câmera")
+#if opcao == "Fazer um video":
+#    st.write("Ainda não é possivel fazer imagens com sua câmera")
 
 if opcao == option1:
     #"Enviar Vídeo Existente":
