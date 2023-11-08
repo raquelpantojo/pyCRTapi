64a31a01d2bcae0ad61f008ce3f5448692cde13f
raquelpantojo <raquel.pantojo.souza@usp.br>
2023-11-08 19:31:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -316,8 +316,8 @@ footer_style = """
 expander = st.expander("+ Informações:")
 with expander:
     st.write("Desenvolvido por:")
-    st.image("logo_lab.png", use_column_width=True, width=50)
-    st.image("logo_usp.png", use_column_width=True, width=50)
+    st.image("logo_lab.png", use_column_width=True, width=25)
+    st.image("logo_usp.png", use_column_width=True, width=25)
 
 # Aplica o estilo ao rodapé
 st.markdown(f'<div style="{footer_style}">{expander}</div>', unsafe_allow_html=True)
